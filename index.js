a6dbf29b246aeb1a8b14ae4a9fd631b015f480fc
Michaël Légaré <gragor30@gmail.com>
2020-09-06 00:19:28 -0400

    spotify support added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { CommandoClient } = require('discord.js-commando');
 const { Structures } = require('discord.js');
 const path = require('path');
-const { prefix, token, discord_owner_id } = require('./config.json');
+const { prefix, token, discord_owner_id, allowed_channels_id } = require('./config.json');
+const allowedChannels = allowed_channels_id.split(",");
 
 Structures.extend('Guild', function(Guild) {
   class MusicGuild extends Guild {
@@ -45,6 +46,11 @@ client.registry
     commandState: false
   })
   .registerCommandsIn(path.join(__dirname, 'commands'));
+
+  //if the command should be blocked because it's not in the right channel.
+  client.dispatcher.addInhibitor(msg => {
+    return allowedChannels.includes(msg.channel.id) ? false : "Unallowed channel";
+  });
 
 client.once('ready', () => {
   console.log('Ready!');
